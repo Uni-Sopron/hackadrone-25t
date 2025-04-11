080d2828d1a6869735c700b0a37dacd760ebeda4
Tamás Szabó <tamas@szaboo.com>
2025-04-11 19:54:44 +0200

    add mock server

---
+++ server/main.mjs
@@ -0,0 +1,15 @@
+import express from 'express'
+import cors from 'cors'
+import fs from 'fs'
+
+const app = express()
+
+app.use(cors())
+
+app.get('/', (req, res) => {
+  const file = fs.readFileSync('./mock.json', 'utf-8')
+  const mock = JSON.parse(file)
+  res.json(mock)
+})
+
+app.listen(3000, () => console.log('listening on port 3000'))
