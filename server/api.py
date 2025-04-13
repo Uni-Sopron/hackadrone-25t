6bc580e36d476ee7ad659bbefb0fa1b00c227bb0
Olivér Ősz <16599031+oliverosz@users.noreply.github.com>
2025-04-13 13:58:08 +0200

    Public api endpoints

--- server/api.py
+++ server/api.py
@@ -10,22 +10,22 @@ from engine.world import Company, Drone, Package, ChargingStation
 api = APIBlueprint("public api", __name__, url_prefix="/api")
 
 
-class DroneBody(BaseModel):
+class DroneRequest(BaseModel):
     company_id: str = Field(..., min_length=1)
     drone_id: str = Field(..., min_length=1)
 
 
-class MoveBody(DroneBody):
+class MoveRequest(DroneRequest):
     latitude: float = Field(..., ge=47, le=48)
     longitude: float = Field(..., ge=16, le=17)
 
 
-class PackageBody(DroneBody):
-    package_id: int = Field(..., ge=0)
+class PackageRequest(DroneRequest):
+    package_id: str = Field(..., min_length=1)
 
 
-class StationBody(DroneBody):
-    station_id: int = Field(..., ge=0)
+class StationRequest(DroneRequest):
+    station_id: str = Field(..., min_length=1, description="Charging station ID")
 
 
 responses: ResponseDict = {
@@ -45,35 +45,100 @@ def state():
 
 
 @api.post("/move")
-def move(body: MoveBody):
+def move(body: MoveRequest):
     """Move the drone to a location"""
-    # TODO
-    return {}
+    action = {
+        "action_type": "drone",
+        "company_id": body.company_id,
+        "drone_id": body.drone_id,
+        "action": "move",
+        "coordinates": [body.latitude, body.longitude],
+    }
+    try:
+        world.action(action)
+        return {}
+    except Exception as e:
+        return {"error": str(e)}, HTTPStatus.BAD_REQUEST
 
 
 @api.post("/pickup", responses=responses)
-def pickup(body: PackageBody):
+def pickup(body: PackageRequest):
     """Pick up a package"""
-    # TODO
-    return ""
+    action = {
+        "action_type": "drone",
+        "company_id": body.company_id,
+        "drone_id": body.drone_id,
+        "action": "pickup_package",
+        "package_id": body.package_id,
+    }
+    try:
+        world.action(action)
+        return {}
+    except Exception as e:
+        return {"error": str(e)}, HTTPStatus.BAD_REQUEST
 
 
 @api.post("/drop", responses=responses)
-def drop(body: PackageBody):
+def drop(body: PackageRequest):
     """Drop off a package"""
-    # TODO
-    return ""
+    action = {
+        "action_type": "drone",
+        "company_id": body.company_id,
+        "drone_id": body.drone_id,
+        "action": "drop_package",
+        "package_id": body.package_id,
+    }
+    try:
+        world.action(action)
+        return {}
+    except Exception as e:
+        return {"error": str(e)}, HTTPStatus.BAD_REQUEST
 
 
 @api.post("/charge", responses=responses)
-def charge(body: StationBody):
+def charge(body: StationRequest):
     """Charge the drone"""
-    # TODO
-    return ""
+    action = {
+        "action_type": "drone",
+        "company_id": body.company_id,
+        "drone_id": body.drone_id,
+        "action": "charge",
+        "charging_station_id": body.station_id,
+    }
+    try:
+        world.action(action)
+        return {}
+    except Exception as e:
+        return {"error": str(e)}, HTTPStatus.BAD_REQUEST
 
 
 @api.post("/rescue", responses=responses)
-def rescue(body: DroneBody):
+def rescue(body: DroneRequest):
     """Rescue the drone"""
-    # TODO
-    return ""
+    action = {
+        "action_type": "drone",
+        "company_id": body.company_id,
+        "drone_id": body.drone_id,
+        "action": "rescue",
+    }
+    try:
+        world.action(action)
+        return {}
+    except Exception as e:
+        return {"error": str(e)}, HTTPStatus.BAD_REQUEST
+
+
+@api.post("/rest", responses=responses)
+def rest(body: DroneRequest):
+    """Rest the drone"""
+    action = {
+        "action_type": "drone",
+        "company_id": body.company_id,
+        "drone_id": body.drone_id,
+        "action": "rest",
+    }
+    try:
+        world.action(action)
+        return {}
+    except Exception as e:
+        return {"error": str(e)}, HTTPStatus.BAD_REQUEST
--- server/instance.py
+++ server/instance.py
@@ -1,13 +1,4 @@
 from engine.world import World
-from engine.utils import Coordinate
 
 world = World()
 print("World initialized.")
-
-world.try_to_register_company("Foo", Coordinate(1.2, 3.4))
-
-world.action({
-    "action_type" : "company",
-    "company_id" : "Foo",
-    "action" : "new_drone"
-})
