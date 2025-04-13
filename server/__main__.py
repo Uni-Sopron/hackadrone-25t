2044ffa91ab42784db968db7afa927b2dbcdadb2
Olivér Ősz <16599031+oliverosz@users.noreply.github.com>
2025-04-13 21:07:25 +0200

    Remove server variable from openapi config

--- server/__main__.py
+++ server/__main__.py
@@ -1,14 +1,12 @@
 import os
 
 from flask_openapi3.models.info import Info
-from flask_openapi3.models.server import Server
 from flask_openapi3.openapi import OpenAPI
 from flask_openapi3.types import SecuritySchemesDict
 
 from .api import api
 from .admin import admin
 
-SERVER_URL = os.getenv("SERVER_URL", "127.0.0.1")
 SERVER_PORT = int(os.getenv("SERVER_PORT", 5000))
 
 api_key = {
@@ -19,12 +17,7 @@ api_key = {
 security_schemes: SecuritySchemesDict = {"Api-Key": api_key}
 
 info = Info(title="HackaDrone API", version="0.1")
-servers = [
-    Server(
-        url=f"https://{SERVER_URL}:{SERVER_PORT}", description="HackaDrone server"
-    )
-]
-app = OpenAPI("HackaDrone server", info=info, servers=servers, security_schemes=security_schemes)
+app = OpenAPI("HackaDrone server", info=info, security_schemes=security_schemes)
 
 app.register_api(api)
 app.register_api(admin)
