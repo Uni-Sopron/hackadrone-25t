7ac5efe6dc63f5e0d181c14e13a7b3c2b08f005c
Tamás Szabó <tamas@szaboo.com>
2025-04-13 02:41:30 +0200

    display team score

--- status/src/components/Teams.jsx
+++ status/src/components/Teams.jsx
@@ -7,26 +7,27 @@ const Color = ({ id }) => {
     <div
       style={{
         borderRadius: '100%',
-        width: 20,
+        width: 24,
         aspectRatio: 1,
-        background: color,
-        marginRight: 5,
-        border: '3px solid white',
-        outline: `1px solid ${color}`,
+        backgroundColor: color,
+        marginRight: 8,
+        border: '2px solid white',
       }}
     />
   )
 }
 
 const Teams = ({ teams }) => {
+  const sortedTeams = [...teams].sort((a, b) => b.score - a.score)
+
   return (
     <div
       style={{
         position: 'absolute',
         bottom: 0,
         left: 0,
-        background: 'rgba(0, 0, 0, 0.5)',
-        color: '#4fc3f7',
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+        color: '#fff',
         width: '100%',
         paddingTop: 10,
         paddingLeft: 10,
@@ -34,11 +35,12 @@ const Teams = ({ teams }) => {
         display: 'flex',
         justifyContent: 'space-evenly',
         flexWrap: 'wrap',
-        backdropFilter: 'blur(5px)',
-        borderTop: '1px solid rgba(255, 255, 255, 0.3)',
+        backdropFilter: 'blur(8px)',
+        borderTop: '1px solid rgba(255, 255, 255, 0.2)',
+        boxShadow: '0 -4px 12px rgba(0, 0, 0, 0.2)',
       }}
     >
-      {teams.map((team) => (
+      {sortedTeams.map((team) => (
         <div
           key={team.teams_id}
           style={{
@@ -46,10 +48,31 @@ const Teams = ({ teams }) => {
             justifyContent: 'center',
             alignItems: 'center',
             marginBottom: 10,
+            padding: 6,
+            transition: 'all 0.2s ease',
           }}
         >
           <Color id={team.teams_id} />
-          {team.name}
+          <div
+            style={{
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              marginTop: 2,
+            }}
+          >
+            <span style={{ fontWeight: 500 }}>{team.name}:</span>
+            <strong
+              style={{
+                marginLeft: '4px',
+                fontSize: '18px',
+                marginTop: 2,
+                color: '#4fc3f7',
+              }}
+            >
+              {team.score}
+            </strong>
+          </div>
         </div>
       ))}
     </div>
