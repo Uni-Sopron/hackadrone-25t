f54561c8faf6df6a7b30b0479c5faedeb7e3d084
Olivér Ősz <16599031+oliverosz@users.noreply.github.com>
2025-04-15 11:12:16 +0200

    Renamed release script, and add Harka station

--- releases/r_0415_900.py
+++ releases/r_0415_900.py
@@ -1,4 +1,4 @@
-from engine.package import Package, generate_random_package, send_package_to_api
+from engine.package import generate_random_package, send_package_to_api
 from engine.utils import Coordinate
 import requests
 
@@ -10,13 +10,30 @@ headers = {
 
 GT = Coordinate(47.6800402, 16.5787537)
 NC = Coordinate(47.6072494, 16.7054558) # Nagycenk, Szechenyi Kastely
+HK = Coordinate(47.6455318, 16.6039671) # Harkai kogli
+
+requests.post(
+    "https://hackadrone.gazd.info/admin/add_station", 
+    json= {
+        "location": GT, 
+        "max_charging_speed_W": 120
+    }, headers=headers    
+)
 
 requests.post(
     "https://hackadrone.gazd.info/admin/add_station", 
     json= {
         "location": NC, 
-        "max_charging_speed_W": 100
+        "max_charging_speed_W": 140
     }, headers=headers    
+)
+
+requests.post(
+    "https://hackadrone.gazd.info/admin/add_station", 
+    json= {
+        "location": HK,
+        "max_charging_speed_W": 30
+    }, headers=headers
 )
 
 # Short, cheap jobs around GT
@@ -34,7 +51,7 @@ for _ in range(5):
 # Short cheap jobs around NC (with longer deadlines)
 for _ in range(5):
     send_package_to_api(generate_random_package(
-        NC, 4000, 4000, 1, 2, 1000, 1200, 5400, 7200
+        NC, 4000, 4000, 1, 2, 1500, 2000, 5400, 7200
     ))
 
 # Expensive jobs, where origin is close to GT, destination is close to NC
@@ -43,8 +60,7 @@ for _ in range(7):
     p = generate_random_package(
         GT, 1000, 1000, 0, 0.1, 5000, 6000, 7500, 9000
     )
-    p.destination[0] += NC[0]-GT[0]
-    p.destination[1] += NC[1]-GT[1]
+    p.destination = Coordinate(p.destination[0] + NC[0] - GT[0], p.destination[1] + NC[1] - GT[1])
     send_package_to_api(p)
 
 # Same, just backwards
@@ -52,7 +68,5 @@ for _ in range(3):
     p = generate_random_package(
         NC, 1000, 1000, 0, 0.1, 5000, 6000, 7500, 9000
     )
-    p.destination[0] += GT[0]-NC[0]
-    p.destination[1] += GT[1]-NC[1]
+    p.destination = Coordinate(p.destination[0] + GT[0] - NC[0], p.destination[1] + GT[1] - NC[1])
     send_package_to_api(p)
-
